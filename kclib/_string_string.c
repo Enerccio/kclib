15869079e73d0dbf08ffc90d7b085acc508ec206
Peter Vanusanik <admin@en-circle.com>
2015-12-30 18:35:27 +0100

    strncpy

--- kclib/_string_string.c
+++ kclib/_string_string.c
@@ -11,3 +11,10 @@ char* strcpy(char* restrict s1, register const char* restrict s2){
 	while ((*(t++) = *(s2++)) != '\0') ;
 	return s1;
 }
+
+char* strncpy(char* restrict s1, register const char* restrict s2, size_t n){
+	register char* t = s1;
+	size_t l = 0;
+	while ((*(t++) = *(s2++)) != '\0' && l<n) ++l;
+	return s1;
+}
