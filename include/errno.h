5e917df565521d3b4ff600cac7e70ca670b772b1
Peter Vanusanik <admin@en-circle.com>
2016-01-09 20:12:19 +0100

    more errnos

--- include/errno.h
+++ include/errno.h
@@ -16,14 +16,23 @@
 extern "C" {
 #endif
 
-#define EDEFAULTERR 0
-#define EDOM		1
-#define EILSEQ		2
-#define ERANGE		3
-#define EIO			4
-#define ENOPROC		5
-#define ENOMEM		6
-#define EINVALID	7
+#define EDEFAULTERR  0
+#define EDOM		 1
+#define EILSEQ		 2
+#define ERANGE		 3
+#define EIO			 4
+#define ENOPROC		 5
+#define ENOMEM		 6
+#define EINVAL	     7
+#define E2BIG		 8
+#define EACCES		 9
+#define	ENAMETOOLONG 10
+#define ENOENT		 11
+#define ENOTDIR		 12
+#define ENOEXEC		 13
+#define ELOOP		 14
+#define ENAMETOOLONG 15
+#define ETXTBSY		 16
 
 extern int err;
 #define errno err
